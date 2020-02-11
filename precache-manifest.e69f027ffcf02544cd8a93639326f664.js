4a0f7e0f50c05ec0c3f9477be3a3b989c01403a1
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-02-11 14:33:10 -0600

    Updates

---
+++ precache-manifest.e69f027ffcf02544cd8a93639326f664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e10912f746c7819dd31",
+    "url": "/periodic-table/static/js/runtime~main.8e10912f.js"
+  },
+  {
+    "revision": "5a4f1beb29c37bed7e99",
+    "url": "/periodic-table/static/js/main.5a4f1beb.chunk.js"
+  },
+  {
+    "revision": "5bc7f1d07bef45b31326",
+    "url": "/periodic-table/static/js/1.5bc7f1d0.chunk.js"
+  },
+  {
+    "revision": "5a4f1beb29c37bed7e99",
+    "url": "/periodic-table/static/css/main.3d089dbb.chunk.css"
+  },
+  {
+    "revision": "4986abb6587ec081dd728d208c59bbbe",
+    "url": "/periodic-table/index.html"
+  }
+];
